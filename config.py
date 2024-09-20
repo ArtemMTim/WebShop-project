8599a332d8291fda6a0da058d7169018acecf5d2
Timofeev Artem <artemtimofeev@rambler.ru>
2024-09-20 19:33:13 +0300

    Некоторые правки.

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 # Корневая директория проекта
 ROOT_DIR = os.path.dirname(__file__)
 
-# Директория для файлов с данными
+# Директория для файлов с данными html страниц
 HTML_DIR = os.path.join(ROOT_DIR, "html")
