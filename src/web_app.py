d9a945b8961b055e373c1fa70ab8c5407c470ed9
Timofeev Artem <artemtimofeev@rambler.ru>
2024-09-20 19:26:20 +0300

    Вэб-страницы и скрипт сервера.

--- src/web_app.py
+++ src/web_app.py
@@ -1,30 +1,34 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from config import HTML_DIR
 
 hostName = "localhost"
 serverPort = 8080
 
+
 class MyServer(BaseHTTPRequestHandler):
+    """Класс, описывающий работу сервера. Дочерний класс класса BaseHTTPRequestHandler."""
+
     def do_GET(self):
-        with open(os.path.join(HTML_DIR, 'contacts.html'), mode='r', encoding='utf-8') as file:
+        """Метод, обрабатывающий GET запросы."""
+        with open(os.path.join(HTML_DIR, "contacts.html"), mode="r", encoding="utf-8") as file:
             data = file.read()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        #self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(data, "utf-8"))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        """Метод, обрабатывающий POST запросы."""
+        content_length = int(self.headers["Content-Length"])
         body = self.rfile.read(content_length)
         print(str(body))
         self.send_response(200)
         self.end_headers()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print(f"Server started http://{hostName}:{serverPort}")
 
